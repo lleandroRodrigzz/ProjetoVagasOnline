2d1fc2544e492a539e76244d37ff53e320092dd7
lleandroRodrigzz <leandromarcoscasse@gmail.com>
2025-11-13 19:24:23 -0300

    Finalizado: IpUtil removido e melhoria nas cores de interface

--- android/app/src/main/java/com/example/vagasapp/configs/RetrofitConfigInteresse.java
+++ android/app/src/main/java/com/example/vagasapp/configs/RetrofitConfigInteresse.java
@@ -1,7 +1,6 @@
 package com.example.vagasapp.configs;
 
 import com.example.vagasapp.services.InteresseService;
-import com.example.vagasapp.utils.IpUtils;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,7 +10,6 @@ public class RetrofitConfigInteresse{
     private final Retrofit retrofit;
 
     public RetrofitConfigInteresse() {
-        //String IPv4 = IpUtils.getLocalIPv4();
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.0.2.2:8080/apis/")
                 .addConverterFactory(GsonConverterFactory.create())
--- android/app/src/main/java/com/example/vagasapp/configs/RetrofitConfigVaga.java
+++ android/app/src/main/java/com/example/vagasapp/configs/RetrofitConfigVaga.java
@@ -1,7 +1,6 @@
 package com.example.vagasapp.configs;
 
 import com.example.vagasapp.services.VagaService;
-import com.example.vagasapp.utils.IpUtils;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,7 +10,6 @@ public class RetrofitConfigVaga {
     private final Retrofit retrofit;
 
     public RetrofitConfigVaga() {
-        //String IPv4 = IpUtils.getLocalIPv4();
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.0.2.2:8080/apis/vagas/")
                 .addConverterFactory(GsonConverterFactory.create())
